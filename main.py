a78091e61156c8454d7c4165a58432bd3037df0b
Boris Kucherov <boris.ku4erov@gmail.com>
2023-04-03 12:46:58 +0300

    add classes Lecturer and Reviewer

--- main.py
+++ main.py
@@ -21,15 +21,21 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
+
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
- 
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
- 
+
 print(best_student.grades)
